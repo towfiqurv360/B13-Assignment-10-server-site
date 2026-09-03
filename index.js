5534a9d1efa4c1252237d70c6c186e4760bdcc66
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-09-03 15:55:06 +0600

    fix(cors): update allowed origin with frontend vercel url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000" ,"https://b13-assignment-10-recipehub.vercel.app"],
   credentials: true 
 }));
 app.use(express.json());
